9aac716659f3eeb2c999460de1fe5235d108a940
jskimweb <jskimweb@gmail.com>
2024-08-19 17:34:26 +0900

    Update font styles

--- src/components/InfoCongest.tsx
+++ src/components/InfoCongest.tsx
@@ -2,7 +2,14 @@ import type { Data } from "../types";
 import "./InfoCongest.css";
 
 const InfoCongest = ({ data }: { data: Data }) => {
-  const { AREA_NM, AREA_CONGEST_LVL, AREA_CONGEST_MSG, PPLTN_TIME } = data;
+  const {
+    AREA_NM,
+    AREA_CONGEST_LVL,
+    AREA_CONGEST_MSG,
+    AREA_PPLTN_MAX,
+    AREA_PPLTN_MIN,
+    PPLTN_TIME,
+  } = data;
   const levelClassName =
     AREA_CONGEST_LVL === "여유"
       ? "level-1"
@@ -15,8 +22,9 @@ const InfoCongest = ({ data }: { data: Data }) => {
   return (
     <div className={`info-congest ${levelClassName}`}>
       <span className="time">{`${PPLTN_TIME} 기준`}</span>
-      <h4 className="spot">{`< ${AREA_NM} >`}</h4>
+      <h4 className="spot">{`${AREA_NM}`}</h4>
       <span className={`level`}>{AREA_CONGEST_LVL}</span>
+      <span className="number">{`${AREA_PPLTN_MIN} ~ ${AREA_PPLTN_MAX} 명`}</span>
       <span className="message">{`"${AREA_CONGEST_MSG}"`}</span>
     </div>
   );
