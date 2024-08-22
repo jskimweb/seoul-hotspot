597642f2b28c770c2aef52c657d698b31d9c53ac
jskimweb <jskimweb@gmail.com>
2024-08-22 11:46:38 +0900

    Fix console error about svg attr name

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -40,9 +40,9 @@ const Search = ({ setSpot }: { setSpot: (spot: string) => void }) => {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 <path d="M18 6 6 18" />
                 <path d="m6 6 12 12" />
